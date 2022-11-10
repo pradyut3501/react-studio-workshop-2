942902c7087d486be361848a4b738eef319f8b2b
lazylion351 <77818347+pradyut3501@users.noreply.github.com>
2022-11-10 18:52:35 -0500

    Finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem.js";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,18 +12,21 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
-
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [items, setItems] = useState([]);
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        <BakeryItem name={item.name} description={item.description} price={item.price} image={item.image}
+        totalPrice={totalPrice} updatePrice={setTotalPrice} items={items} setItems={setItems}/>// replace with BakeryItem component
       ))}
 
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        {items.map((item) => {return <p>{item}</p>;})}
+        <h3>Total Price = ${totalPrice}</h3>
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,20 @@
 // TODO: create a component that displays a single bakery item
+export default function BakeryItem(props) {
+
+    const handleClick = () => {
+        props.setItems([...props.items, props.name])
+        props.updatePrice(props.totalPrice + props.price)
+    }
+
+	return (
+		<div class="BakeryItem">
+			<h3>{props.name}</h3>
+            <p>{props.description}</p>
+            <p>{props.price}</p>
+            <img src={props.image} width="200rem" height="150rem"></img>
+            <br></br>
+            <button onClick={handleClick} class="button">Add Item</button>
+		</div>
+	);
+}
+
